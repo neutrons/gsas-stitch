3f25e71c34c0f71fc86bb6e99f68664227dc801c
Pete Peterson <petersonpf@ornl.gov>
2015-04-28 10:37:18 -0400

    First pass at rpm generation

---
+++ setup.py
@@ -0,0 +1,44 @@
+try:
+  # Use easy setup to ensure dependencies
+  import ez_setup
+  ez_setup.use_setuptools()
+except ImportError:
+  pass
+
+from distutils.core import setup
+
+
+package_name = "mergegsas"
+version      = "0.1.0" # TODO currently hand-synchronized
+author       = "Peter F. Peterson"
+license      = "The MIT License"
+email        = "petersonpf@ornl.gov"
+url          = "https://github.com/neutrons/gsas-stitch"
+description  = "Script to combine two or more powgen gsas files for use in pdfgetn"
+
+scripts = ["scripts/mergegsas"]
+py_modules = []
+package_dir = []
+packages = []
+package_data = {}
+data_files = []
+extensions_modules=[] # written in C
+requires=[]
+options={}
+
+setup(name=package_name,
+      version=version,
+      description=description,
+      author=author,
+      author_email=email,
+      url=url,
+      scripts=scripts,
+      py_modules=py_modules,
+      ext_modules=extensions_modules,
+      packages=packages,
+      package_dir=package_dir,
+      package_data=package_data,
+      data_files=data_files,
+      requires=requires, #does not do anything
+      **options
+     )
